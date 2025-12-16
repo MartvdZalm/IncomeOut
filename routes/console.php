3769786a9500aeea4fed97fd851245b45d2dc027
Mart van der Zalm <martvanderzalm@gmail.com>
2025-12-16 16:13:56 +0100

    Schedule recurring transactions command

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Schedule recurring transactions to be processed daily
 Schedule::command('recurring:process')->daily();
